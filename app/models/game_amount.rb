c4eb67f23dbbf67c289b1c89c709684b1d11a397
sleeping-lion <toughjjh@gmail.com>
2016-06-07 18:59:42 +0900

    update

--- app/models/game_amount.rb
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-
-class GameAmount < ActiveRecord::Base
-  attr_accessible :amount
-  validates_presence_of :amount
-  validates :amount, :numericality => { :only_integer => true }
-end
--- app/models/game_setting.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-class GameSetting < ActiveRecord::Base
-  attr_accessible :id, :title, :percentage, :amount
-  validates_presence_of :title, :percentage, :amount
-  validates :amount, :numericality => { :only_integer => true }
-  #validates :percentage
-  has_many :game
-end
