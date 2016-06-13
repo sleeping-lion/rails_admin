666315cd38e46f0067fbe90d789c4d8c0d3a62a3
sleeping-lion <toughjjh@gmail.com>
2016-06-13 23:36:17 +0900

    update

--- app/models/attendance_event_prize.rb
+++ app/models/attendance_event_prize.rb
@@ -1,8 +1,5 @@
-# encoding: utf-8
-
-class AttendancePicture < ActiveRecord::Base
-  attr_accessible :attendance_setting_id, :start_date, :title, :flag, :photo, :photo_cache
-  validates_presence_of :attendance_setting_id, :start_date, :title
-  belongs_to :attendance_setting, :autosave=>true
+class AttendanceEventPrize < ActiveRecord::Base
+  attr_accessible  :title, :photo, :flag
+  belongs_to :attendance
   mount_uploader :photo, AttendanceUploader  
 end
